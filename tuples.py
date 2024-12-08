25f75932e25ac32ded1f0c27b3f4e634f36d379e
Harry23457 <southwelle@hotmail.com>
2024-12-08 08:41:04 +0000

    Add files via upload
    t=(10,20,30)
    print (t)
    print(type(t))
    print(t[1])
    #t[1]=40
    #slicing
    print(t[0:2])
    
    address=(45,"mouse street","london",134893)
    denum,area,city,zip=address
    print (city) 
    print (denum)

---
+++ tuples.py
@@ -0,0 +1,12 @@
+t=(10,20,30)
+print (t)
+print(type(t))
+print(t[1])
+#t[1]=40
+#slicing
+print(t[0:2])
+
+address=(45,"mouse street","london",134893)
+denum,area,city,zip=address
+print (city) 
+print (denum)
