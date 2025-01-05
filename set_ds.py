6414762134f0d1c8f61ca7eb8c3d85777e5af37f
Harry23457 <southwelle@hotmail.com>
2025-01-05 09:00:53 +0000

    Add files via upload

---
+++ set_ds.py
@@ -0,0 +1,14 @@
+"""student_id={34,46,64,34,85,25,64}
+print (student_id)
+print (type(student_id))
+
+student_id.add(78)
+print (student_id)
+names=["kate","oliver","harry","loki"]
+student_id.update(names)
+print (student_id)
+student_id.discard("kate")
+print (student_id)
+
+print (len (student_id))
+"""
