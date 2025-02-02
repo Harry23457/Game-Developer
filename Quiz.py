622f988a0cee170473b55e4fa4eda69a446c9502
Harry23457 <southwelle@hotmail.com>
2025-02-02 08:56:43 +0000

    Add files via upload

--- Quiz.py
+++ Quiz.py
@@ -6,11 +6,12 @@ HEIGHT = 650
 
 questions=[]
 
+score = 0
 currentQ=0
 totalQ=0
 smg=()
 timeq=15
-
+finished=False
 
 scrollerbox = Rect(0,0,870,80)
 questionbox = Rect(20,100,650,150)
@@ -34,11 +35,11 @@ def draw():
     smg=f"welcome to quizmaster. Q:{currentQ}:{totalQ}"
     screen.draw.textbox(smg,scrollerbox,color="white")
     screen.draw.textbox(str(timeq),timerbox,color="white")
-    screen.draw.textbox(q[0],questionbox,color="white")
+    screen.draw.textbox(q[0].strip(),questionbox,color="white")
     screen.draw.textbox("skip",skipbox,color = "white",angle = -90)
     index=1
     for i in optlist:
-        screen.draw.textbox(q[index],i,color ="white")
+        screen.draw.textbox(q[index].strip(),i,color ="white")
         index += 1
 
 def update():
@@ -58,14 +59,46 @@ def nextque():
     currentQ += 1
     return questions.pop(0).split("|")
 
+def on_mouse_down(pos):
+    index=1
+    for i in optlist:
+        if i.collidepoint(pos):
+            if index is int(q[5]):
+                correctanwser()
+            else:
+                gameover()
+
+        index+=1
+
+def correctanwser():
+    global timeq, score, q
+    score += 1
+    if questions:
+        q=nextque()
+        timeq=15
+    else :
+        gameover()
 
 
 
+def gameover():
+    global timeq, finished, q, score
+    message=f"game over you got {score} questions correct"
+
+    q=[message,"-","-","-","-",0]
+    timeq=0
+    finished=True
+
+def timer():
+    global timeq
+    if timeq:
+        timeq = timeq - 1
+    else:
+        gameover()
+ 
 
 
-
-
-
+clock.schedule_interval(timer,1)
 loadque()
 q=nextque()
 print (questions[0])
